ba0e384fc75c4104d873692c46f7026c0fecd5f6
Juan F Gonzalez <juano_gonza@hotmail.com>
2018-09-21 18:54:56 -0500

    changes for connect and jwt

--- src/routes.php
+++ src/routes.php
@@ -6,16 +6,15 @@ use \Firebase\JWT\JWT;
 
 //Traer cupos 
 $app->get('/api', function (Request $request, Response $response) {
-    // Sample log message
-    //$this->logger->info("Slim-Skeleton '/api' route");
+
+    $this->logger->info("Slim-Skeleton '/api' route");
     $arr = array();
     $mysqli = conect();
-    $query = "SELECT * FROM tncupo";
+    $query = "SELECT * FROM tcupos";
     if ($resul = $mysqli->query($query)) {
         while ($row = $resul->fetch_assoc()) {
             $arr[] = $row;
         }
-        //$response = json_encode($arr);
     }
     $newResponse = $response->withJson($arr);
     return $newResponse->withHeader('Content-type', 'application/json;charset=utf-8');
@@ -23,13 +22,15 @@ $app->get('/api', function (Request $request, Response $response) {
 
 //Agregar estudiante
 $app->post('/api/estudiante', function (Request $request, Response $response) {
-    // Sample log message
-    //$this->logger->info("Slim-Skeleton '/api' route");
+
+    $this->logger->info("Slim-Skeleton '/api/estudiante' route");
+    $input = $request->getParsedBody();
     $mysqli = conect();
     $query = "CALL addEstud(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     //retrieve passw from request body and pass it to password_hash() function
+    $hash = password_hash($input['password'], PASSWORD_DEFAULT);
     if ($stmt = $mysqli->prepare($query)) {
-        //$stmt->bind_param('issisisisis', );
+        $stmt->bind_param('issisisisis', $input['reserva'], $input['nombre'], $input['codigo'], $input['carrera'], $input['semestre'], $input['email'], $input['documento'], $hash ,$input['bloqueado'], $input['observacion']);
         $stmt->execute();
         $stmt->close();
     }
@@ -37,34 +38,40 @@ $app->post('/api/estudiante', function (Request $request, Response $response) {
 
 //Agregar horario
 $app->post('/api/horario', function (Request $request, Response $response) {
-    // Sample log message
-    //$this->logger->info("Slim-Skeleton '/api' route");
+ 
+    $this->logger->info("Slim-Skeleton '/api/horario' route");
+    $input = $request->getParsedBody();
     $mysqli = conect();
     $query = "CALL addHorario(?, ?, ?)";
     if ($stmt = $mysqli->prepare($query)) {
-        //$stmt->bind_param('sss', );
+        $stmt->bind_param('iss', $input['hora'], $input['dia'], $input['email']);
         $stmt->execute();
         $stmt->close();
+        echo $mysqli->info;
     }
 });
 
-//transform this function from pdo to oo mysqli 
+//Loguearse al sistema 
 $app->post('/login', function (Request $request, Response $response, array $args) {
 
     $input = $request->getParsedBody();
-    $sql = "SELECT * FROM testudiantes WHERE email= :email";
-    $sth = $this->db->prepare($sql);
-    $sth->bindParam("email", $input['email']);
-    $sth->execute();
-    $user = $sth->fetchObject();
- 
+    $arr = array();
+    $mysqli = conect();
+    $query = "SELECT * FROM testudiantes WHERE email = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("s", $input['email']);
+    $stmt->execute();
+    $resul = $stmt->get_result();
+    while ($row = $resul->fetch_object("Student")) {
+        $arr[] = $row;
+    }
     // verify email address.
-    if (!$user) {
+    if (!$arr) {
         return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);
     }
  
     // verify password.
-    if (!password_verify($input['password'], $user->password)) {
+    if (!password_verify($input['password'], $arr->password)) {
         return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);
     }
 
